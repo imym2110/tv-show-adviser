af296853d5fb8635e059fa7972defed5684ca68e
imym2110 <61817551+imym2110@users.noreply.github.com>
2024-07-10 11:28:45 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://api.themoviedb.org/3/";
+const BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY_PARAM = "?api_key=336df96fbd5d30fd78d14188a3d6ca12";
 const BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/original";
 const SMALL_IMAGE_COVER_BASE_URL = "https://image.tmdb.org/t/p/w300";
